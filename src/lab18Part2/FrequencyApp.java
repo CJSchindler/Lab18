f97885c7709d046a0c22a2abcbb6c1124bbf0e10
Lauren Shindo <lauren.shindo@gmail.com>
2018-08-08 08:49:21 -0400

    added Big-O and array list

--- src/lab18Part2/FrequencyApp.java
+++ src/lab18Part2/FrequencyApp.java
@@ -1,5 +1,8 @@
+//Lauren Shindo
+
 package lab18Part2;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,19 +12,39 @@ public class FrequencyApp {
 
 		int[] problemArray = {1, 2, 3, 2, 2, 4, 5, 5, 7, 8, 4, 4, 1, 0, 10};
 		
-		Map <Integer, Integer> freqMap = new HashMap<>();
+		//to line 27 is the solution using arrays
+		//estimated Big-O: O(n)
+		int[] resultArray = new int[11];
+		
+		Arrays.fill(resultArray, 0);
 		
 		for (int entry : problemArray) {
-	          
-			if(freqMap.containsKey(entry)) {
-				freqMap.put(entry, freqMap.get(entry)+1);
-	        }
-	        else {
-	        	freqMap.put(entry, 1);
-	        }
+			resultArray[entry]++;
 		}
 		
-		System.out.println(freqMap);
+		for (int i = 0; i < resultArray.length; i++) {
+			System.out.print("[" + i + "]: ");
+			System.out.println(resultArray[i]);
+		}
+		
+		}
+		
+		//all of the below is for the HashMap version of the exercise:
+		//estimated Big-O: O(n)
+	
+//		Map <Integer, Integer> freqMap = new HashMap<>();
+//		
+//		for (int entry : problemArray) {
+//	          
+//			if(freqMap.containsKey(entry)) {
+//				freqMap.put(entry, freqMap.get(entry)+1);
+//	        }
+//	        else {
+//	        	freqMap.put(entry, 1);
+//	        }
+//		}
+		
+//		System.out.println(freqMap);
 	}
 
-}
+//}
